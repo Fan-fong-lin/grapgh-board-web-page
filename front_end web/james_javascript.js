69218cac5f8ded4bd86b470f999645c3262798b2
James Fan <9515308james@gmail.com>
2022-12-06 22:19:50 +0800

    first commit

---
+++ front_end web/james_javascript.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>javascript 的基本練習</title>
+    </head>
+    <body>
+        <script>
+            consolo.log("hello javascript");
+        </script>
+    </body>
+
+</html>
